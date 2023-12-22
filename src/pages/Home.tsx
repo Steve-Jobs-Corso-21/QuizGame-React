99ffe9bdc7b922b617b30cd4dcfbb294eb42b883
Flame0510 <micheletornello5@gmail.com>
2023-12-22 13:12:29 +0100

    feat(counter): add counter component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Counter from "../components/Counter";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <Counter />
+        </div>
+    );
+};
+
+export default Home;
