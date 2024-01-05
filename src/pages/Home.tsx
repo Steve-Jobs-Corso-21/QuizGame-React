b134b15357efaf38e4dd25c60c2ae3ac3d8e5d06
Davide Visco <davidevisco@email.com>
2024-01-05 17:00:26 +0100

    feat(Quiz): add global stat base behaviour

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,32 @@
-import Counter from "../components/Counter";
+import { Link } from "react-router-dom";
+
+
+export type Data = {
+    "isTraining": boolean,
+    "currentLevel": string,
+    "quizzes": {
+        [key: string]: string[]
+    },
+    "rightAnswers": {
+        [key: string]: string[]
+    }
+}
 
 const Home = () => {
+    const data: Data = {
+        "isTraining": true,
+        "currentLevel": "mondo1",
+        "quizzes": {
+            "mondo1": ["1","2","3"]
+        },
+        "rightAnswers": {
+        }
+    }
+
     return (
         <div>
             <h1>Home</h1>
-            <Counter />
+            <Link to={`/quiz/${data.quizzes["mondo1"][0]}`} state={data}>Gioca</Link>
         </div>
     );
 };
--- src/pages/Quiz/index.tsx
+++ src/pages/Quiz/index.tsx
@@ -1,28 +1,70 @@
-import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams, useLocation } from "react-router-dom";
 import "./index.scss";
 import data from "../../data.json";
-import { useState } from "react";
+import { Data } from "../Home";
 import Timer from "./Timer";
 import Modal from "../../components/Modal";
 
+// TODO: Unable back to already responded questions
+// TODO: Testo che cambia in base alla lunghezza della domanda stessa
+// TODO: Centrare le risposte verticalmente, se non c'è una immagine
+// TODO: Se è allenamento, non mostrare la modale, ma continua a fare rispondere fino alla risposta corretta, se le sbaglia tutte l'ultima è sicuro quella giusta
+// se è giusta mostra la modale
+
+
+
+// /quiz/:id
 const Quiz = () => {
-    const { id } = useParams();
-
-    const [correct, setCorrect] = useState<boolean | null>(null);
-    const [stopTimer, setStopTimer] = useState<boolean>(false);
-
-    const quiz = data.quizzes.find(({ id: quizId }) => quizId === id);
-
+    // change this to change max time for answer a question (in seconds)
+    const MAXTIME = 10;
+    // change this to change modal id
     const explainModalID = "explainModal";
 
+    // get id from url
+    const { id } = useParams();
+
+    // called at load of question
+    useEffect(() => {
+        // reset time
+        setStopTimer(false);
+        setMaxTime(MAXTIME);
+    }, [id]);
+
+    // get data through pages 
+    const { state }: { state: Data } = useLocation();
+    const currentLevel = state.currentLevel;
+    const isTraining = state.isTraining;
+    const quizzes = state.quizzes[currentLevel];
+    const currentQuiz = quizzes.findIndex((q: string) => q === id);
+
+    // hooks
+    const [correct, setCorrect] = useState<boolean | null>(null);
+    const [stopTimer, setStopTimer] = useState<boolean>(false);
+    const [maxTime, setMaxTime] = useState(MAXTIME);
+
+    // get right quiz
+    const quiz = data.quizzes.find(({ id: quizId }) => quizId === id);
+
+    // handle answer click
     const answerClick = (correct: boolean) => {
         setCorrect(correct);
         setStopTimer(true);
+        // setta variabile con correct
     }
 
     return (
         <div>
-            <Modal modalID={explainModalID} isCorrect={correct} description={quiz?.description} url={`/quiz/2`}></Modal>
+            {/* {isTraining ? correct && (
+
+            ) : (
+
+            )} */}
+
+            <Modal modalID={explainModalID} isCorrect={correct}
+                description={quiz?.description}
+                url={currentQuiz >= quizzes.length - 1 ? "/stats" : `/quiz/${quizzes[currentQuiz + 1]}`}
+                state={state}></Modal>
 
             { /* // TODO : ICON PACK DA METTERE */}
             <nav className="mx-4 nav d-flex align-items-center justify-content-center">
@@ -38,11 +80,8 @@ const Quiz = () => {
 
             {quiz && (
                 <div>
-
                     <h2 className="d-flex align-items-center justify-content-center mt-3 pt-3 fs-1">{quiz.question}</h2>
-
                     <div className="d-flex align-items-center justify-content-between row m-0 px-5">
-
                         <div className={`d-flex flex-wrap col-lg-${quiz.image ? 6 : 12}`}>
                             {quiz.answers.map(({ answer, correct }) => (
                                 <button className={`text-align-center btn btn-primary btn-block btn-custom col-${quiz.image ? 12 : 4}`}
@@ -52,18 +91,17 @@ const Quiz = () => {
                                 </button>
                             ))}
                         </div>
-
                         {quiz.image && (
                             <div className="d-flex align-items-center justify-content-center col-5 overflow-hidden">
                                 <img className="w-100" src={quiz.image} alt={quiz.question} />
                             </div>
                         )}
                     </div>
-
-                    
                     { /* //! Noi non abbiamo il tasto per il prossimo livello <Link to="/quiz/2">VAI AL PROSSIMO</Link> */}
 
-                    <Timer maxTime={10} stopTimer={stopTimer} modalID={explainModalID}></Timer>
+                    {!isTraining && (
+                        <Timer quizID={id} maxTime={maxTime} stopTimer={stopTimer} modalID={explainModalID}></Timer>
+                    )}
                 </div>
             )}
 
@@ -73,6 +111,3 @@ const Quiz = () => {
 };
 
 export default Quiz;
-
-// TODO: Testo che cambia in base alla lunghezza della domanda stessa
-// TODO: Centrare le risposte verticalmente, se non c'è una immagine
