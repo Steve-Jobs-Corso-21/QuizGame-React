00f1027e6b873ae256015a50904d088283b92aac
HappyUserFree <g.barbag93@gmail.com>
2024-01-06 13:18:49 +0100

    merge Peppe build() epilogue e score

---
+++ src/pages/HomeEpilogue.tsx
@@ -0,0 +1,17 @@
+import { Link, useParams, useSearchParams } from "react-router-dom";
+import Epilogue from "./Epilogue";
+
+const HomeEpilogue = () => {
+  return (
+    <div>
+      <h1> Home del riepilogo</h1>
+      <Epilogue></Epilogue>
+
+      <Link to="/" style={{ display: "flex", justifyContent: "flex-start" }}>
+        Vai alla Home
+      </Link>
+    </div>
+  );
+};
+
+export default HomeEpilogue;
---
+++ src/pages/HomeScore.tsx
@@ -0,0 +1,14 @@
+import { Link, useParams, useSearchParams } from "react-router-dom";
+import Score from "./Score";
+
+const HomeEpilogue = () => {
+  return (
+    <div>
+      <h1> Home dello score</h1>
+      <Score></Score>
+      <Link to="/" style={{ display: "flex", justifyContent: "flex-start" }}>
+        Vai alla Home
+      </Link>
+    </div>
+  );
+};
