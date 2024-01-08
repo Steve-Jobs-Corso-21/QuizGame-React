f65317e0d476cfa37a2c9170be84b6aafeb6b1d5
Domenico Ventura <domesalmo@outlook.it>
2024-01-08 13:00:33 +0100

    feat(menu): dinamic mod choose and bootstrap layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,20 +4,24 @@ import "./home.scss";
 import data from "../data.json";
 import { useState } from "react";
 
+// modalità allenamento di default ?
+// altrimenti bloccare tasto gioca se non è scelta la modalità
 const Menu = () => {
-
-    let x : boolean;
+    const [y,sety] = useState<string>("");
+    const modeClick = (mod:string) => {
+        sety(mod);
+    } 
     
     return (
         <>
-        <div className="mainMenu">
+        <div className="mainMenu d-flex flex-column align-items-center justify-content-around vh-100">
             <h1>Cyberquiz</h1>
-            <h2>Scegli la modalita'</h2>
-            <div>
-                <button onClick={() => {x = true}}>Allenamento</button>
-                <button onClick={() => {x = false}}>Sfida</button>
+            <div className="box-mod d-flex flex-column align-items-center mt-5 px-5 py-2">
+                <h2>Scegli la modalita'</h2>
+                <button type="button" className={`btn-custom btn btn-primary p-3 m-2 ${y === "allenamento" && "active"}`} data-bs-toggle="button" aria-pressed={y === "allenamento" ? "true" : "false"} onClick={() => modeClick("allenamento")} >Allenamento</button>
+                <button type="button" className={`btn-custom btn btn-primary p-3 m-2 ${y === "sfida" && "active"}`} data-bs-toggle="button" aria-pressed={y === "sfida" ? "true" : "false"} onClick={() => modeClick("sfida")} >Sfida</button>
             </div>
-            <div className="buttonGioca">
+            <div className="btn-custom btn btn-primary p-3">
                 <Link to="/map">Gioca</Link>
             </div>
         </div>
