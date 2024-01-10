d1c848a6aeaacc0dbf5c1e42ca9e3ecd5201c980
riccardobondisi <riccardobondisi@gmail.com>
2024-01-10 20:35:54 +0100

    feat() Dynamic Modal

--- src/pages/Question/index.tsx
+++ src/pages/Question/index.tsx
@@ -1,10 +1,79 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './style.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.css";
+import data from "../../data.json";
 
+export enum GameMode {
+  Training,
+  Challenge,
+}
+
+export type Data = {
+  gameMode: GameMode;
+  currentLevel: string;
+  quizzes: {
+    [key: string]: string[];
+  };
+  rightAnswers: {
+    [key: string]: {
+      [key: string]: number[];
+    };
+  };
+};
+
+type Answer = {
+  answer: string;
+  correct?: boolean;
+};
+
+type Quiz = {
+  id: string;
+  question: string;
+  description: string;
+  image?: string;
+  answers: Answer[];
+};
+
+type Quizzes = {
+  [key: string]: Quiz[];
+};
+
+export type JSON = {
+  quizzes: Quizzes;
+};
+
+const json: JSON = data;
+
+const testAnswer: Data = {
+  gameMode: GameMode.Training,
+  currentLevel: "mondo1",
+  quizzes: {
+    mondo1: ["1", "2", "3"],
+  },
+  rightAnswers: {
+    mondo1: {
+      "1": [2],
+      "4": [0],
+      "5": [1],
+      "6": [3],
+    },
+  },
+};
 
 const Question = () => {
-  const [risposte, setRisposte] = useState(Array(10).fill(false));
+  const dataKeys = Object.keys(
+    testAnswer.rightAnswers[testAnswer.currentLevel]
+  ).length;
+
+  //   const testTryAnswer =  (testAnswer.gameMode===GameMode.Challenge) ?
+  // Object.entries(testAnswer.rightAnswers[testAnswer.currentLevel]).reduce((acc, s) => acc +=
+  //   json.quizzes[testAnswer.currentLevel].find(({ id: quizID }: { id: string }) =>
+  // s[0] === quizID)?.answers.findIndex(({ correct }) => correct) === s[1][0] ? 100/Object.keys(testAnswer.rightAnswers[testAnswer.currentLevel]).length : 0, 0)
+
+  // Riprendere da qui
+  // const text = data.quizzes[testAnswer.currentLevel];
+
+  const [risposte, setRisposte] = useState(Array(dataKeys).fill(false));
 
   const toggleRisposta = (index: number) => {
     const newRisposte = [...risposte];
@@ -14,7 +83,8 @@ const Question = () => {
 
   return (
     <div className="accordion" id="domandeAccordion">
-      {Array.from({ length: 10 }, (_, index) => (
+      {/* index=0 */}
+      {Array.from({ length: dataKeys }, (_, index) => (
         <div className="accordion-item" key={index}>
           <h2 className="accordion-header" id={`domandaHeading${index}`}>
             <button
@@ -22,7 +92,7 @@ const Question = () => {
               type="button"
               data-bs-toggle="collapse"
               data-bs-target={`#domandaCollapse${index}`}
-              aria-expanded={risposte[index] ? 'true' : 'false'}
+              aria-expanded={risposte[index] ? "true" : "false"}
               aria-controls={`domandaCollapse${index}`}
               onClick={() => toggleRisposta(index)}
             >
@@ -31,7 +101,9 @@ const Question = () => {
           </h2>
           <div
             id={`domandaCollapse${index}`}
-            className={`accordion-collapse collapse ${risposte[index] ? 'show' : ''}`}
+            className={`accordion-collapse collapse ${
+              risposte[index] ? "show" : ""
+            }`}
             aria-labelledby={`domandaHeading${index}`}
             data-bs-parent="#domandeAccordion"
           >
@@ -40,7 +112,11 @@ const Question = () => {
               <p>Descrizione della domanda {index + 1}...</p>
 
               {/* Stato della risposta */}
-              <p className={risposte[index] ? 'text-success' : 'text-danger'}>{risposte[index] ? 'Risposta corretta!' : 'Risposta non data o errata.'}</p>
+              <p className={risposte[index] ? "text-success" : "text-danger"}>
+                {risposte[index]
+                  ? "Risposta corretta!"
+                  : "Risposta non data o errata."}
+              </p>
             </div>
           </div>
         </div>
