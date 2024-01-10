403c2af95b40b53ba90f2fdeb8710422f9f8ecb6
riccardobondisi <riccardobondisi@gmail.com>
2024-01-10 12:56:29 +0100

    feat() Score Progression - first try

--- src/pages/Epilogue/index.tsx
+++ src/pages/Epilogue/index.tsx
@@ -2,14 +2,73 @@ import { Link, useParams, useSearchParams } from "react-router-dom";
 import "./index.scss";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import testAnswer from "../../testAnswer.json";
+// import testAnswer from "../../testAnswer.json";
 import { useState } from "react";
+import { GameMode } from "../CircularProgressBar";
 
-const sumRightAfter = testAnswer.rightAnswers.mondo1.reduce(
-  (acc, s) => (acc += s.rightAfter),
-  0
-);
+// const sumRightAfter = testAnswer.rightAnswers.mondo1.reduce(
+//   (acc, s) => (acc += s.rightAfter),
+//   0
+// );
 
+export type Data = {
+  gameMode: GameMode;
+  currentLevel: string;
+  quizzes: {
+    [key: string]: string[];
+  };
+  rightAnswers: {
+    [key: string]: {
+      [key: string]: string[];
+    };
+  };
+};
+
+type Answer = {
+  answer: string;
+  correct?: boolean;
+};
+
+type Quiz = {
+  id: string;
+  question: string;
+  description: string;
+  image?: string;
+  answers: Answer[];
+};
+const testAnswer: Data = {
+  gameMode: GameMode.Training,
+  currentLevel: "mondo1",
+  quizzes: {
+    mondo1: ["1", "2", "3"],
+  },
+  rightAnswers: {
+    mondo1: {
+      "1": ["abc", "ced", "lsd"],
+      "2": ["abc", "ced", "lsd"],
+      "3": ["abc", "ced", "lsd"],
+      "4": ["abc"],
+      "5": ["abc", "ced"],
+      "6": ["abc", "ced", "lsd", "hjk"],
+      "7": ["abc", "ced"],
+      "8": ["abc", "ced", "lsd"],
+      "9": ["abc", "ced"],
+      "10": ["abc"],
+    },
+    mondo2: {
+      "1": ["abc", "ced", "lsd"],
+      "2": ["abc", "ced", "lsd", "abc", "ced", "lsd"],
+      "3": ["abc", "ced", "lsd", "lsd"],
+      "4": ["abc"],
+      "5": ["abc", "ced"],
+      "6": ["abc", "ced", "lsd", "abc", "ced", "lsd", "abc", "ced", "lsd"],
+      "7": ["abc", "ced", "lsd", "abc", "ced", "lsd", "abc"],
+      "8": ["abc", "ced", "lsd", "abc", "ced", "lsd"],
+      "9": ["abc", "ced"],
+      "10": [],
+    },
+  },
+};
 const Epilogue = () => {
   return (
     <div>
@@ -24,11 +83,9 @@ const Epilogue = () => {
         Complimenti! Hai terminato il gioco!
       </h1>
       <div>
+        <p>Sei riuscito a completare il gioco facendo {} errori!</p>
         <p>
-          Sei riuscito a completare il gioco facendo {sumRightAfter} errori!
-        </p>
-        <p>
-          {testAnswer.isTraining
+          {testAnswer.gameMode === GameMode.Training
             ? "Hai giocato in modalità allenamento... Che ne diresti di passare alla modalità sfida?"
             : "Complimenti per aver terminato la modalità sfida!"}
         </p>
