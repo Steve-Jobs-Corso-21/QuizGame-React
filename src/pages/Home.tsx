b60bd40b5809f6ccc6567abf17f46745e2513499
Domenico Ventura <domesalmo@outlook.it>
2024-01-11 12:17:30 +0100

    feat(): icon btn-info added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import "./Quiz/index.scss";
 import "./home.scss";
 import { useState } from "react";
 import Modal from "../components/Modal";
+import Header from "../components/Header";
 import { title } from "process";
 
 export enum GameMode {
@@ -77,16 +78,23 @@ const Menu = () => {
         >
             
         </Modal>
+        <Header
+            htmlBlock={""}
+            audio={true}
+            audioURL={""}
+        />
             <div className="mainMenu d-flex flex-column align-items-center justify-content-around vh-100">
                 <h1>Cyberquiz</h1>
                 <div className="box-mod d-flex flex-column align-items-center mt-5 px-5 py-2">
-                    <h2 className="">Scegli la modalita'</h2>
-                    <button className="btn-info btn-primary"
-                    data-bs-toggle="modal"
-                    data-bs-target="#11"
-                    >
-                        i
-                    </button>
+                    <div className="d-flex">
+                        <h2 className="me-5">Scegli la modalita'</h2>
+                        <button className="btn-info btn-primary "
+                        data-bs-toggle="modal"
+                        data-bs-target="#11"
+                        >
+                            <img src="info-circle.svg" alt="" />
+                        </button>
+                    </div>
                     <Link
                         className="btn-custom-home btn btn-primary p-3 m-2"
                         to="/map"
