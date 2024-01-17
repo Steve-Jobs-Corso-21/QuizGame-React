68f8c87fc4522565d6274408a14972972373ea15
HappyUserFree <g.barbag93@gmail.com>
2024-01-17 11:12:15 +0100

    feat() epilogue

--- src/pages/Epilogue/index.tsx
+++ src/pages/Epilogue/index.tsx
@@ -1,13 +1,33 @@
 import { Link, useLocation } from "react-router-dom";
-import {JSON, Data, GameMode} from "../Home";
+import { JSON, Data, GameMode } from "../Home";
 import data from "../../questions.json";
 import "./index.scss";
 
 const Epilogue = () => {
-  const { state } : {state: Data} = useLocation();
+  const { state }: { state: Data } = useLocation();
   const json: JSON = data;
+  const scoreEpilogue = state.rightAnswers.reduce(
+    (totAcc, map, index) =>
+      (totAcc += map.quiz.reduce(
+        (acc: number, q: { id: string; answers: number[] }) =>
+          (acc +=
+            q.answers[0] ===
+            json.maps[index].quizzess
+              .find(({ id }) => id === q.id)
+              ?.answers.findIndex(({ correct }) => correct)
+              ? 0
+              : 1),
+        0
+      )),
+    0
+  );
+  const NumberOfQuestions = data.maps.reduce(
+    (acc, s) => (acc += s.quizzess.length),
+    0
+  );
 
-  console.log(state);
+  const result =
+    ((NumberOfQuestions - scoreEpilogue) / NumberOfQuestions) * 100;
 
   return (
     <div>
@@ -22,19 +42,23 @@ const Epilogue = () => {
         Complimenti! Hai Finito il gioco!
       </h1>
       <div>
-        <p>
-          Sei riuscito a completarlo facendo {
-              state.rightAnswers.reduce((totAcc, map, index) =>
-                totAcc += map.quiz.reduce((acc: number, q: {id: string, answers: number[]}) =>
-                  acc += (q.answers[0] === json.maps[index].quizzess.find(({id}) => id === q.id)?.answers.findIndex(({correct}) => correct)) ? 0 : 1
-                , 0)
-              , 0)
-            } errori!
-        </p>
+        <p>Sei riuscito a completarlo facendo {scoreEpilogue} errori!</p>
         <p>
           {state.gameMode === GameMode.Training
             ? "Hai giocato in modalità allenamento... Che ne diresti di passare alla modalità sfida?"
             : "Complimenti per aver terminato la modalità sfida!"}
+        </p>
+      </div>
+      <div className="d-flex flex-column align-items-center justify-content-center ">
+        <p className="justify-content-end">
+          Complimenti! sei ai livelli di un:
+          {result < 25
+            ? "Hacker"
+            : result < 50
+            ? "Esperto della cybersecurity"
+            : result < 75
+            ? "Principiante della Cybersecurity"
+            : "Neofita della cybersecurity"}
         </p>
       </div>
       <div>
--- src/pages/Score/index.tsx
+++ src/pages/Score/index.tsx
@@ -1,10 +1,11 @@
 import "./index.scss";
 import "react-circular-progressbar/dist/styles.css";
-import { Link } from "react-router-dom";
-import { useLocation } from "react-router";
+import { Link } from "react-router-dom/dist";
+import { useLocation } from "react-router/dist";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import AnimatedProgressProvider from "../../components/AnimatedProgressProvider";
 import { easeExpOut } from "d3-ease";
+import { easeBackInOut } from "d3-ease";
 import { JSON, GameMode, Data } from "../Home";
 import data from "../../questions.json";
 import Header from "../../components/Header";
@@ -12,34 +13,46 @@ import Modal from "../../components/Modal";
 import Question from "../Question";
 
 const Score = () => {
-  const { state } : {state: Data} = useLocation();
-  const json : JSON = data;
+  const { state }: { state: Data } = useLocation();
+  const json: JSON = data;
 
-  const scoring = state.gameMode === GameMode.Training
-    ? state.rightAnswers[state.currentLevel].quiz.filter(({ answers }) => answers.length === 1).length * (100 / state.rightAnswers[state.currentLevel].quiz.length)
-    : state.rightAnswers[state.currentLevel].quiz.filter(({id} : {id: string}) => 
-      state.rightAnswers[state.currentLevel].quiz.find(({id : quizID} : {id: string})=> quizID === id)!.answers.length > 0 &&
-      json.maps[state.currentLevel].quizzess.find(({id : quizID} : {id: string})=> quizID === id)?.answers.findIndex(({correct}) => correct) === state.rightAnswers[state.currentLevel].quiz.find(({id : quizID} : {id: string})=> quizID === id)!.answers[0]
-    ).length * (100 / state.rightAnswers[state.currentLevel].quiz.length);
+  const scoring =
+    state.gameMode === GameMode.Training
+      ? state.rightAnswers[state.currentLevel].quiz.filter(
+          ({ answers }) => answers.length === 1
+        ).length *
+        (100 / state.rightAnswers[state.currentLevel].quiz.length)
+      : state.rightAnswers[state.currentLevel].quiz.filter(
+          ({ id }: { id: string }) =>
+            state.rightAnswers[state.currentLevel].quiz.find(
+              ({ id: quizID }: { id: string }) => quizID === id
+            )!.answers.length > 0 &&
+            json.maps[state.currentLevel].quizzess
+              .find(({ id: quizID }: { id: string }) => quizID === id)
+              ?.answers.findIndex(({ correct }) => correct) ===
+              state.rightAnswers[state.currentLevel].quiz.find(
+                ({ id: quizID }: { id: string }) => quizID === id
+              )!.answers[0]
+        ).length *
+        (100 / state.rightAnswers[state.currentLevel].quiz.length);
 
   // console.log(scoring);
 
-  const color = scoring === 100
-    ? "rgba(45, 124, 255, 1)"
-    : scoring > 50
+  const color =
+    scoring === 100
+      ? "rgba(45, 124, 255, 1)"
+      : scoring > 50
       ? "rgba(45, 198, 46, 1)"
       : scoring > 25
-        ? "rgba(255, 209, 0,1)"
-        : "rgba(255, 124, 0, 1)";
-
+      ? "rgba(255, 209, 0,1)"
+      : "rgba(255, 124, 0, 1)";
 
   return (
     <div>
-      <Modal modalID="answerModal"
-        description={
-          <Question />
-        }
-        canClose = {true}
+      <Modal
+        modalID="answerModal"
+        description={<Question />}
+        canClose={true}
         state={state}
       />
 
@@ -48,7 +61,9 @@ const Score = () => {
       <div className="d-flex container flex-column">
         <div className="d-flex align-items-center justify-content-center">
           <h1 className="text-center">
-            Complimenti! Hai terminato il {state.gameMode === GameMode.Training && "training del"} mondo {json.maps[state.currentLevel].name.toUpperCase()}
+            Complimenti! Hai terminato il{" "}
+            {state.gameMode === GameMode.Training && "training del"} mondo{" "}
+            {json.maps[state.currentLevel].name.toUpperCase()}
           </h1>
         </div>
 
@@ -57,27 +72,55 @@ const Score = () => {
             <h3>Percentuale risposte esatte:</h3>
             <div style={{ width: "300px" }}>
               <AnimatedProgressProvider
-              valueStart={0}
-              valueEnd={scoring}
-              duration={1.4}
-              easingFunction={easeExpOut}>
-                {(value: number) => 
-                  <CircularProgressbar value={value} text={`${value.toFixed(0)}`} minValue={0} maxValue={100} styles={buildStyles({
-                    pathColor: color,
-                    textColor: color,
-                  })}/>
-                }
+                valueStart={0}
+                valueEnd={scoring}
+                duration={1.4}
+                easingFunction={easeBackInOut}
+              >
+                {(value: number) => (
+                  <CircularProgressbar
+                    value={value}
+                    text={`${value.toFixed(0)}`}
+                    minValue={0}
+                    maxValue={100}
+                    styles={buildStyles({
+                      pathColor: color,
+                      textColor: color,
+                    })}
+                  />
+                )}
               </AnimatedProgressProvider>
             </div>
           </div>
 
           <div className="d-flex flex-column align-items-center justify-content-center">
-            <button className="btn btn-secondary btn-lg m-3" data-bs-target={"#answerModal"} data-bs-toggle="modal">Vedi le tue risposte</button>
+            <button
+              className="btn btn-secondary btn-lg m-3"
+              data-bs-target={"#answerModal"}
+              data-bs-toggle="modal"
+            >
+              Vedi le tue risposte
+            </button>
             {/* <Link to="/" className="btn btn-secondary btn-lg m-3" state={state}>Torna alla home</Link> */}
-            {state.currentLevel === json.maps.length - 1 && state.gameMode === GameMode.Challenge
-              ? <Link to="/epilogue" className="btn btn-secondary btn-lg m-3" state={state}>Termina gioco</Link>
-              : <Link to="/map" className="btn btn-secondary btn-lg m-3" state={state} onClick={() => state.currentLevel++}>Torna alla mappa</Link>
-            }
+            {state.currentLevel === json.maps.length - 1 &&
+            state.gameMode === GameMode.Challenge ? (
+              <Link
+                to="/epilogue"
+                className="btn btn-secondary btn-lg m-3"
+                state={state}
+              >
+                Termina gioco
+              </Link>
+            ) : (
+              <Link
+                to="/map"
+                className="btn btn-secondary btn-lg m-3"
+                state={state}
+                onClick={() => state.currentLevel++}
+              >
+                Torna alla mappa
+              </Link>
+            )}
           </div>
         </div>
       </div>
